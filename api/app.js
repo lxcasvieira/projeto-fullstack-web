e1ffa1544e2ff6d44833b0cc45a08b7cbe209a8d
sabsunflower <sabrinamatias228@gmail.com>
2022-09-02 10:21:05 -0300

    atualizando

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // Carregar Rotas
-var usuario_rotas = require(/rota/usuario);
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
